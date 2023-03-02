a172bd737daa4316e3e517773cb6352f5287cc4f
wirabudhi <bpantaiselatan@gmail.com>
2023-03-02 11:24:04 +0800

    Change Binding

--- app/src/main/java/com/example/barvolume/MainActivity.kt
+++ app/src/main/java/com/example/barvolume/MainActivity.kt
@@ -6,63 +6,54 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.barvolume.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    // Define view
-    private lateinit var edtWidth: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var edtLength: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
 
     // Define Variable
     companion object {
         private const val STATE_RESULT = "state_result"
     }
 
+    private lateinit var binding: ActivityMainBinding
+
     // Define function onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        edtLength = findViewById(R.id.edt_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
-
-        btnCalculate.setOnClickListener(this)
+        binding.btnCalculate.setOnClickListener(this)
 
         if (savedInstanceState != null) {
             val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text = result
+            binding.tvResult.text = result
         }
     }
 
     // Define function onClick
     override fun onClick(v: View?) {
         if (v?.id == R.id.btn_calculate) {
-            val inputLength = edtLength.text.toString().trim()
-            val inputWidth = edtWidth.text.toString().trim()
-            val inputHeight = edtHeight.text.toString().trim()
+            val inputLength = binding.edtLength.text.toString().trim()
+            val inputWidth = binding.edtWidth.text.toString().trim()
+            val inputHeight = binding.edtHeight.text.toString().trim()
             var isEmptyFields = false
 
             if (inputLength.isEmpty()) {
                 isEmptyFields = true
-                edtLength.error = "Field ini tidak boleh kosong"
+                binding.edtLength.error = "Field ini tidak boleh kosong"
             }
             if (inputWidth.isEmpty()) {
                 isEmptyFields = true
-                edtWidth.error = "Field ini tidak boleh kosong"
+                binding.edtWidth.error = "Field ini tidak boleh kosong"
             }
             if (inputHeight.isEmpty()) {
                 isEmptyFields = true
-                edtHeight.error = "Field ini tidak boleh kosong"
+                binding.edtHeight.error = "Field ini tidak boleh kosong"
             }
             if (!isEmptyFields) {
                 val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-                tvResult.text = volume.toString()
+                binding.tvResult.text = volume.toString()
             }
         }
     }
@@ -70,7 +61,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     // Define function
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, tvResult.text.toString())
+        outState.putString(STATE_RESULT, binding.tvResult.text.toString())
     }
 
 }
